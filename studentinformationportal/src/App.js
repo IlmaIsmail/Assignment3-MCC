2b9123f0b858be2310b6b7074424d7b85b4d3cd2
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-19 17:20:04 +0000

    Profile Component added

--- studentinformationportal/src/App.js
+++ studentinformationportal/src/App.js
@@ -18,7 +18,7 @@ function App() {
         </tbody>
         </table>
       
-      <hr/>
+      
     </div>
   );
 }
---
+++ studentinformationportal/src/components/Profile.js
@@ -0,0 +1,14 @@
+export default function portalrofile(props){
+    return(
+        <>
+        <table>
+            <tr>
+                <td rowSpan={7}>
+                    <img src={requestAnimationFrame("../assets/img/img1.jpg")} alt="profile picture"/>
+                </td>
+                <td>ID</td>
+            </tr>
+        </table>
+        </>
+    )
+}
