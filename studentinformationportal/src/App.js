1ec4669a934530f498afd181218c7db53f0bd48b
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-19 17:08:18 +0000

    adjusments

--- studentinformationportal/src/App.js
+++ studentinformationportal/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1 align="center">Student Information portal</h1>
       <hr/>
-      <table width="100">
+      <table width="100%">
         <tbody>
           <tr>
             <td>
